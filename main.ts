0d96d8783c111de0de5b772c4d9c88d3b4b2bc5d
Mykola Sliepchenko <slepchenko.nicholas@gmail.com>
2025-12-24 15:19:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,36 @@
 input.onButtonPressed(Button.A, function () {
     robotbit.GeekServo(robotbit.Servos.S1, -50)
+    robotbit.GeekServo(robotbit.Servos.S2, -50)
     robotbit.GeekServo(robotbit.Servos.S3, -50)
+    robotbit.GeekServo(robotbit.Servos.S4, -50)
     robotbit.GeekServo(robotbit.Servos.S5, -50)
+    robotbit.GeekServo(robotbit.Servos.S6, -50)
     robotbit.GeekServo(robotbit.Servos.S7, -50)
+    robotbit.GeekServo(robotbit.Servos.S8, -50)
 })
 input.onButtonPressed(Button.B, function () {
     robotbit.GeekServo(robotbit.Servos.S1, 290)
+    robotbit.GeekServo(robotbit.Servos.S2, 290)
     robotbit.GeekServo(robotbit.Servos.S3, 290)
+    robotbit.GeekServo(robotbit.Servos.S4, 290)
     robotbit.GeekServo(robotbit.Servos.S5, 290)
+    robotbit.GeekServo(robotbit.Servos.S6, 290)
     robotbit.GeekServo(robotbit.Servos.S7, 290)
+    robotbit.GeekServo(robotbit.Servos.S8, 290)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     for (let angle = -60; angle <= 300; angle++) {
         robotbit.GeekServo(robotbit.Servos.S1, angle)
+        robotbit.GeekServo(robotbit.Servos.S2, angle)
         robotbit.GeekServo(robotbit.Servos.S3, angle)
+        robotbit.GeekServo(robotbit.Servos.S4, angle)
         robotbit.GeekServo(robotbit.Servos.S5, angle)
+        robotbit.GeekServo(robotbit.Servos.S6, angle)
         robotbit.GeekServo(robotbit.Servos.S7, angle)
+        robotbit.GeekServo(robotbit.Servos.S8, angle)
         basic.pause(100)
     }
 })
 basic.forever(function () {
-    robotbit.GeekServo2KG(robotbit.Servos.S1, 180)
+	
 })
